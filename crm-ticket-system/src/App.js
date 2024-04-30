a1b17489b31069882c948f35e7621d187a2ca978
Patric Shingi <patricchamu@gmail.com>
2024-04-30 15:16:06 +0200

    dashboard page added

--- crm-ticket-system/src/App.js
+++ crm-ticket-system/src/App.js
@@ -4,12 +4,15 @@ import "./App.css";
 
 import { Entry } from './pages/entry/Entry.page';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/dashboard/Dashboard.page';
 
 function App() {
   return (
   <div className='App'>
     {/* <Entry /> */}
-    <DefaultLayout>//Dashboard</DefaultLayout>
+    <DefaultLayout>
+      <Dashboard/>
+    </DefaultLayout>
   </div>
   );
 }
